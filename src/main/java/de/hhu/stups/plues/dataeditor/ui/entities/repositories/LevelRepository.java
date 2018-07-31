6953b1e1b71592c5abff7041a8cd2d06b2c52f74
Happe, Christopher (chhap101) <christopher.happe@uni-duesseldorf.de>
2018-07-31 17:18:05 +0200

    Cleanup LevelRepository

--- src/main/java/de/hhu/stups/plues/dataeditor/ui/entities/repositories/LevelRepository.java
+++ src/main/java/de/hhu/stups/plues/dataeditor/ui/entities/repositories/LevelRepository.java
@@ -4,7 +4,6 @@ import de.hhu.stups.plues.dataeditor.ui.entities.Level;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface LevelRepository extends CrudRepository<Level,Integer> {
@@ -18,22 +17,16 @@ public interface LevelRepository extends CrudRepository<Level,Integer> {
         + "VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)",
         nativeQuery = true)
   @Modifying
-  void insertSimpleLevel(@Param("id") int id, @Param("name") String name, @Param("tm") String tm,
-                         @Param("art") String art, @Param("min") int min, @Param("max") int max,
-                         @Param("min_credit_points") int minCp,
-                         @Param("max_credit_points") int maxCp,
-                         @Param("parent_id") Integer parentId);
+  void insertSimpleLevel(int id, String name, String tm, String art, int min, int max,
+                         int minCp, int maxCp, Integer parentId);
 
   @Transactional
-  @Query(value = "update levels set name = :name, tm = :tm, art = :art, min = :min, max = :max, "
-        + "min_credit_points = :min_credit_points, max_credit_points = :max_credit_points, "
-        + "parent_id = :parent_id where id = :id", nativeQuery = true)
+  @Query(value = "update levels set name = ?2, tm = ?3, art = ?4, min = ?5, max = ?6, "
+        + "min_credit_points = ?7, max_credit_points = ?8, "
+        + "parent_id = ?9 where id = ?1", nativeQuery = true)
   @Modifying
-  void updateSimpleLevel(@Param("id") int id, @Param("name") String name, @Param("tm") String tm,
-                         @Param("art") String art, @Param("min") int min, @Param("max") int max,
-                         @Param("min_credit_points") int minCp,
-                         @Param("max_credit_points") int maxCp,
-                         @Param("parent_id") Integer parentId);
+  void updateSimpleLevel(int id, String name, String tm, String art, int min, int max,
+                         int minCp, int maxCp, Integer parentId);
 
   @Transactional
   @Modifying
